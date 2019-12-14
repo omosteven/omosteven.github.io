a573f24efa0fb464b37f3c9765a9a140909bb688
omosteven <omosteven123@gmail.com>
2019-12-14 18:57:57 +0100

    Add files via upload

--- my-dashboard/bundle.js
+++ my-dashboard/bundle.js
@@ -1,22 +1,3 @@
-var web3= require('web3');
-
-
-
-if (typeof web3 !== 'undefined') {
-    // Use Mist/MetaMask'npm s provider
-    web3js = new Web3(web3.currentProvider);
-  } else {
-    console.log('No web3? You should consider trying MetaMask!')
-    // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
-    web3js = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
-  }
-  web3.eth.getCoinbase(function(err, account) {
-    if (err === null) {
-      App.account = account;
-      $("#accountAddress").html("Your Account: " + account);
-alert('Your public address is' +account);  
-    }
-
 webpackJsonp([0], {
     132: function(t, i, a) {
         "use strict";
