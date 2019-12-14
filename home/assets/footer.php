ec3c7f575f4dffd7a316f3c2f9a28170e746d637
omosteven <omosteven123@gmail.com>
2019-12-14 15:10:59 +0100

    Add files via upload

---
+++ home/assets/footer.php
@@ -0,0 +1,95 @@
+<footer class="footer-area section_gap" style="background-color:rgb(0,0,30);color:white;">
+		<div class="container">
+			<div class="row" style="font-weight: bold;">
+				<div class="col-lg-6  col-md-6 col-sm-6">
+					<div class="single-footer-widget">
+						<h6 class="footer_title" style="color:white">FRESHLYCHOSEN</h6>
+						<p>25350 White Fence Ct, Acampo California(CA), 95220.</p>
+					</div>
+				</div>
+				<div class="col-lg-6  col-md-6 col-sm-6">
+					<div class="single-footer-widget">
+						<h6 class="footer_title" style="color:white">HOURS</h6>
+						<p>Monday - Friday 9am to 6 pm</p>
+						<p>contact@freshlychosen.com</p>
+					</div>
+				</div>
+			</div>
+				<!-- <div class="col-lg-6 col-md-6 col-sm-6">
+					<div class="single-footer-widget">
+						<h6 class="footer_title">Newsletter</h6>
+						<p>Stay updated with our latest trends</p>
+						<div id="mc_embed_signup">
+							<form target="_blank" action="https://spondonit.us12.list-manage.com/subscribe/post?u=1462626880ade1ac87bd9c93a&amp;id=92a4423d01"
+							 method="get" class="subscribe_form relative">
+								<div class="input-group d-flex flex-row">
+									<input name="EMAIL" placeholder="Email Address" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Email Address'"
+									 required="" type="email">
+									<button class="btn sub-btn">
+										<span class="lnr lnr-arrow-right"></span>
+									</button>
+								</div>
+								<div class="mt-10 info"></div>
+							</form>
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-3 col-md-6 col-sm-6">
+					<div class="single-footer-widget instafeed">
+						<h6 class="footer_title">Instagram Feed</h6>
+						<ul class="list instafeed d-flex flex-wrap">
+							<li>
+								<img src="../../img/instagram/Image-01.jpg" alt="">
+							</li>
+							<li>
+								<img src="../../img/instagram/Image-02.jpg" alt="">
+							</li>
+							<li>
+								<img src="../../img/instagram/Image-03.jpg" alt="">
+							</li>
+							<li>
+								<img src="../../img/instagram/Image-04.jpg" alt="">
+							</li>
+							<li>
+								<img src="../../img/instagram/Image-05.jpg" alt="">
+							</li>
+							<li>
+								<img src="../../img/instagram/Image-06.jpg" alt="">
+							</li>
+							<li>
+								<img src="../../img/instagram/Image-07.jpg" alt="">
+							</li>
+							<li>
+								<img src="../../img/instagram/Image-08.jpg" alt="">
+							</li>
+						</ul>
+					</div>
+				</div>
+				<div class="col-lg-2 col-md-6 col-sm-6">
+					<div class="single-footer-widget f_social_wd">
+						<h6 class="footer_title">Follow Us</h6>
+						<p>Let us be social</p>
+						<div class="f_social">
+							<a href="http://www.facebook.com">
+								<i class="fa fa-facebook"></i>
+							</a>
+							<a href="http://www.twitter.com">
+								<i class="fa fa-twitter"></i>
+							</a>
+							<a href="http://www.instagram.com">
+								<i class="fa fa-instagram"></i>
+							</a>
+							<a href="http://www.linkedin.com">
+								<i class="fa fa-linkedin"></i>
+							</a>
+						</div>
+					</div>
+				</div>
+			</div> -->
+			<div style="margin-top:2em" class="row d-flex justify-content-between align-items-center">
+				<p class="col-lg-12 footer-text text-center">
+COPYRIGHT &copy;<script>document.write(new Date().getFullYear());</script>, FRESHLYCHOSEN
+				</p>
+			</div>
+		</div>
+</footer>
---
+++ home/header.php
@@ -0,0 +1,276 @@
+<header class="fl-header">
+
+            <!-- Header Top Start -->
+            <div class="header-top-area d-none d-lg-block">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-12">
+                            <div class="header-top-inner">
+                                <div class="row">
+                                    <div class="col-lg-4 col-md-3">
+                                        <div class="social-top">
+                                            <ul>
+                                                <li><a href="#"><i class="ion-social-facebook"></i></a></li>
+                                                <li><a href="#"><i class="ion-social-twitter"></i></a></li>
+                                                <li><a href="#"><i class="ion-social-tumblr"></i></a></li>
+                                                <li><a href="#"><i class="ion-social-googleplus"></i></a></li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-8 col-md-9">
+                                        <div class="top-info-wrap text-right">
+                                            <ul class="top-info">
+                                                <li>Mon - Fri : 9am to 5pm </li>
+                                                <li><a href="#">+2347037048119</a></li>
+                                                <li><a href="#">foodelv@gmail.com</a></li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- Header Top End -->
+
+            <!-- haeader bottom Start -->
+            <div class="haeader-bottom-area header-sticky">
+                <div class="container">
+                    <div class="row align-items-center">
+                        <div class="col-lg-2 col-md-4 col-5">
+                            <div class="logo-area" style="font-size:0.5em;font-family: sans-serif">
+                                <a href="" style="width:10em"><img src="images/foodev" alt="FooDev"></a>
+                                <!-- <i style="color:red">Food</i><i style="color:rgb(0,0,70)">Delv</i> -->
+                            </div>
+                        </div>
+                        <div class="col-lg-8 d-none d-lg-block">
+                            <div class="main-menu-area text-center">
+                                <!--  Start Mainmenu Nav-->
+                                <nav class="main-navigation">
+                                    <ul>
+                                        <li><a href="">Home</a></li>
+                                        
+                                        <li><a href="shop.html">Pages</a>
+                                            <ul class="mega-menu">
+                                                <li><a href="#">Column One</a>
+                                                    <ul>
+                                                        <li><a href="compare.html">Compare Page</a></li>
+                                                        <li><a href="login-register.html">Login &amp; Register</a></li>
+                                                        <li><a href="my-account.html">My Account Page</a></li>
+                                                    </ul>
+                                                </li>
+                                                <li><a href="blog.html">Column two</a>
+                                                    <ul>
+                                                        <li><a href="product-details.html">Product Details 1</a></li>
+                                                        <li><a href="product-details-2.html">Product Details 2</a></li>
+                                                        <li><a href="checkout.html">Checkout Page</a></li>
+                                                    </ul>
+                                                </li>
+                                                <li><a href="#">Column Three</a>
+                                                    <ul>
+                                                        
+                                                        <li><a href="cart.html">Cart Page</a></li>
+                                                        <li><a href="wishlist.html">Wish List Page</a></li>
+                                                    </ul>
+                                                </li>
+                                            </ul>
+                                        </li>
+                                        <li><a href="#">shop</a>
+                                            <ul class="sub-menu">
+                                                <li><a href="shop.html">Shop Left Sidebar</a></li>
+                                                <li><a href="shop-right.html">Shop Right Sidebar</a></li>
+                                                <li><a href="shop-fullwidth.html">Shop Full Width</a></li>
+                                            </ul>
+                                        </li>
+                                        <li><a href="about-us.html">About</a></li>
+                                        <li><a href="contact-us.html">Contact</a></li>
+                                    </ul>
+                                </nav>
+
+                            </div>
+                        </div>
+
+                        <div class="col-lg-2 col-md-8 col-7">
+                            <div class="right-blok-box d-flex">
+                                <div class="search-wrap">
+                                    <a href="#" class="trigger-search"><i class="ion-ios-search-strong"></i></a>
+                                </div>
+
+                                <div class="user-wrap">
+                                    <a href="wishlist.html"><i class="ion-android-favorite-outline"></i></a>
+                                </div>
+
+
+                                <div class="shopping-cart-wrap">
+                                    <a href="#"><i class="ion-ios-cart-outline"></i> <span id="cart-total">2</span></a>
+                                    <ul class="mini-cart">
+                                        <li class="cart-item">
+                                            <div class="cart-image">
+                                                <a href="product-details.html"><img alt="" src="assets/images/product/product-01.jpg"></a>
+                                            </div>
+                                            <div class="cart-title">
+                                                <a href="product-details.html">
+                                                    <h4>Product Name 01</h4>
+                                                </a>
+                                                <span class="quantity">1 ×</span>
+                                                <div class="price-box"><span class="new-price">$130.00</span></div>
+                                                <a class="remove_from_cart" href="#"><i class="icon-trash icons"></i></a>
+                                            </div>
+                                        </li>
+                                        <li class="cart-item">
+                                            <div class="cart-image">
+                                                <a href="product-details.html"><img alt="" src="assets/images/product/product-02.jpg"></a>
+                                            </div>
+                                            <div class="cart-title">
+                                                <a href="product-details.html">
+                                                    <h4>Product Name 03</h4>
+                                                </a>
+                                                <span class="quantity">1 ×</span>
+                                                <div class="price-box"><span class="new-price">$130.00</span></div>
+                                                <a class="remove_from_cart" href="#"><i class="icon-trash icons"></i></a>
+                                            </div>
+                                        </li>
+                                        <li class="subtotal-titles">
+                                            <div class="subtotal-titles">
+                                                <h3>Sub-Total :</h3><span>$ 230.99</span>
+                                            </div>
+                                        </li>
+                                        <li class="mini-cart-btns">
+                                            <div class="cart-btns">
+                                                <a href="cart.html">View cart</a>
+                                                <a href="checkout.html">Checkout</a>
+                                            </div>
+                                        </li>
+                                    </ul>
+                                </div>
+
+                                <div class="mobile-menu-btn d-block d-lg-none">
+                                    <div class="off-canvas-btn">
+                                        <i class="ion-android-menu"></i>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- haeader bottom End -->
+
+            <!-- main-search start -->
+            <div class="main-search-active">
+                <div class="sidebar-search-icon">
+                    <button class="search-close"><span class="ion-android-close"></span></button>
+                </div>
+                <div class="sidebar-search-input">
+                    <form>
+                        <div class="form-search">
+                            <input id="search" class="input-text" value="" placeholder="Search entire store here ..." type="search">
+                            <button class="search-btn" type="button">
+                                <i class="ion-ios-search"></i>
+                            </button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+            <!-- main-search start -->
+
+
+            <!-- off-canvas menu start -->
+            <aside class="off-canvas-wrapper">
+                <div class="off-canvas-overlay"></div>
+                <div class="off-canvas-inner-content">
+                    <div class="btn-close-off-canvas">
+                        <i class="ion-android-close"></i>
+                    </div>
+
+                    <div class="off-canvas-inner">
+
+                        <!-- mobile menu start -->
+                        <div class="mobile-navigation">
+
+                            <!-- mobile menu navigation start -->
+                            <nav>
+                                <ul class="mobile-menu">
+                                    <li class="menu-item-has-children"><a href="#">Home</a>
+                                        <ul class="dropdown">
+                                            <li><a href="index.html">Home version 01</a></li>
+                                            <li><a href="index-2.html">Home version 02</a></li>
+                                        </ul>
+                                    </li>
+                                    <li class="menu-item-has-children"><a href="#">pages</a>
+                                        <ul class="megamenu dropdown">
+                                            <li class="mega-title has-children"><a href="#">Column One</a>
+                                                <ul class="dropdown">
+                                                    <li><a href="compare.html">Compare Page</a></li>
+                                                    <li><a href="login-register.html">Login &amp; Register</a></li>
+                                                    <li><a href="my-account.html">My Account Page</a></li>
+                                                </ul>
+                                            </li>
+                                            <li class="mega-title has-children"><a href="#">Column two</a>
+                                                <ul class="dropdown">
+                                                    <li><a href="product-details.html">Product Details 1</a></li>
+                                                    <li><a href="product-details-2.html">Product Details 2</a></li>
+                                                    <li><a href="checkout.html">Checkout Page</a></li>
+                                                </ul>
+                                            </li>
+                                            <li class="mega-title has-children"><a href="#">Column Three</a>
+                                                <ul class="dropdown">
+                                                    <li><a href="error404.html">Error 404</a></li>
+                                                    <li><a href="cart.html">Cart Page</a></li>
+                                                    <li><a href="wishlist.html">Wish List Page</a></li>
+                                                </ul>
+                                            </li>
+                                        </ul>
+                                    </li>
+                                    <li class="menu-item-has-children "><a href="#">shop</a>
+                                        <ul class="dropdown">
+                                            <li><a href="shop.html">Shop Left Sidebar</a></li>
+                                            <li><a href="shop-right.html">Shop Right Sidebar</a></li>
+                                            <li><a href="shop-fullwidth.html">Shop Full Width</a></li>
+                                        </ul>
+                                    </li>
+                                   
+                                    <li><a href="about-us.html">About</a></li>
+                                    <li><a href="contact-us.html">Contact</a></li>
+                                </ul>
+                            </nav>
+                            <!-- mobile menu navigation end -->
+                        </div>
+                        <!-- mobile menu end -->
+
+
+
+                        <!-- offcanvas widget area start -->
+                        <div class="offcanvas-widget-area">
+                            <div class="off-canvas-contact-widget">
+                                <ul>
+                                    <li>
+                                        Mon - Fri : 9am to 5pm
+                                    </li>
+                                    <li>
+                                        <a href="#">0123456789</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">info@yourdomain.com</a>
+                                    </li>
+                                </ul>
+                            </div>
+                            <div class="off-canvas-social-widget">
+                                <a href="#"><i class="ion-social-facebook"></i></a>
+                                <a href="#"><i class="ion-social-twitter"></i></a>
+                                <a href="#"><i class="ion-social-tumblr"></i></a>
+                                <a href="#"><i class="ion-social-googleplus"></i></a>
+                            </div>
+
+                        </div>
+                        <!-- offcanvas widget area end -->
+                    </div>
+                </div>
+            </aside>
+            <!-- off-canvas menu end -->
+
+
+        </header>
